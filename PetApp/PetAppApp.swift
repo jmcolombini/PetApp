7bb1ea9ab855fdcbec2aa571200169d80e038969
JM <jmccardonha@outlook.com>
2025-01-29 20:28:36 -0300

    Initial Commit

---
+++ PetApp/PetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAppApp.swift
+//  PetApp
+//
+//  Created by João Marcelo Colombini Cardonha on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetAppTests/PetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetAppTests.swift
+//  PetAppTests
+//
+//  Created by João Marcelo Colombini Cardonha on 29/01/25.
+//
+
+import Testing
+@testable import PetApp
+
+struct PetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
